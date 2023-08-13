d2451dd5e9f6b0cfcce20fafea2bc8462128f9db
Deshan Dharmasena <deshan@dxdy.tech>
2023-08-13 20:30:22 +0530

    register success message

--- src/app/components/auth/signup/signup.component.ts
+++ src/app/components/auth/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AlertService } from 'src/app/shared/alert.service';
 import { AuthService } from 'src/app/shared/auth.service';
 import { TokenService } from 'src/app/shared/token.service';
 
@@ -15,10 +16,11 @@ export class SignupComponent {
   loading: boolean = false;
 
   constructor(
-    public router: Router,
-    public fb: FormBuilder,
-    public token : TokenService,
-    public authService: AuthService
+    private router: Router,
+    private fb: FormBuilder,
+    private token : TokenService,
+    private authService: AuthService,
+    private alert: AlertService
   ) {
     this.registerForm = this.fb.group({
       first_name: [''],
@@ -44,6 +46,7 @@ export class SignupComponent {
         this.loading = false
       },
       () => {
+        this.alert.success('You have successfully registered')
         this.registerForm.reset();
         this.loading = false
         this.router.navigate(['sign-in']);
